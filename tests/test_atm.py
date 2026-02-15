8cd5721e898ca5e504593365cd1ab738ca70c66d
Jorge <103543373+AustralCoder@users.noreply.github.com>
2026-02-14 23:38:55 -0300

    refactor test: added fixtures for all test written so far

--- tests/test_atm.py
+++ tests/test_atm.py
@@ -1,63 +1,72 @@
 from ATM_Practice import ATM, Account, Technician, Administration
 import pytest
 
+@pytest.fixture
+def nico():
+    nico = Account(_pin=2026, name="Nico", _balance=500, _active=True)
+    return nico
+
+@pytest.fixture
+def blocked_user():
+    blocked_user = Account(_pin=2026, name="blocked", _balance=500, _active=False)
+    return blocked_user
+
+@pytest.fixture
+def atm_new_york():
+    cajero = ATM("New York", _cash_inventory=1000, _admin_key = 129)
+    return cajero
+
+@pytest.fixture
+def atm_technician():
+    atm_technician = Technician("George", 129)
+    return atm_technician
+
+@pytest.fixture
+def atm_admin():
+    atm_admin= Administration("Johnny Guitar", 1234)
+    return atm_admin
 
 # ATM CLASS TESTS
 
-def test_account_string_representation():
-
-    nico = Account(_pin=2026, name="Nico", _balance=0, _active=True)
-    blocked_user = Account(_pin=2026, name="blocked", _balance=0, _active=False)
+def test_account_string_representation(nico, blocked_user):
 
     assert str(nico) == "You are Nico, your account is currently online"
     assert "currently disabled" in str(blocked_user)
 
+def test_cash_inventory_is_protected(atm_new_york):
 
-def test_cash_inventory_is_protected():
-    cajero = ATM("New York", _cash_inventory=1000)
-
-    assert cajero.cash_inventory == 1000
+    assert atm_new_york.cash_inventory == 1000
     with pytest.raises(AttributeError):
-        cajero.cash_inventory = 5000
+        atm_new_york.cash_inventory = 5000
 
-def test_initial_balance():
-
-    nico = Account(_pin=2026, name="Nick", _balance=500, _active=True)
+def test_initial_balance(nico):
     assert nico.balance == 500
 
-def test_change_power_status():
+def test_change_power_status(atm_new_york):
+    atm_new_york.change_power_status(129, new_status= False)
 
-    atm = ATM("New Jersey", 10000, _is_active=True, _admin_key= 129)
-    atm.change_power_status(129, new_status= False)
+    assert atm_new_york._is_active == False
 
-    assert atm._is_active == False
-
-def test_withdraw_balance():
-
-    cajero = ATM("New York", _admin_key = 1234)
-
-    nico = Account(_pin=2026, name="Nick", _balance=40000, _active=True)
-
-    cajero.withdraw(nico, 1, 2026)
-
-    assert nico.balance == 39999
-
-def test_atm_reload():
-
-    cajero = ATM("New York", _admin_key = 1234)
-
-    jorge = Technician("Jorge", 1234)
-
-    jorge.reload_atm(cajero, 1)
-
-    assert cajero.cash_inventory == 500001
-
-def test_block_user_from_atm():
-    cajero = ATM("New York", _admin_key = 1234)
-    Johnny = Administration("Johnny Guitar", 1234)
-    nico = Account(_pin=2026, name="Nick", _balance=0, _active=True)
+def test_withdraw_balance(atm_new_york, nico):
     
-    Johnny.block_user(cajero, nico)
+    atm_new_york.withdraw(nico, 1, 2026)
+    assert nico.balance == 499
+
+def test_atm_reload(atm_technician, atm_new_york):
+
+    atm_technician.reload_atm(atm_new_york, 1)
+
+    assert atm_new_york.cash_inventory == 1001
+
+def test_denied_atm_reload(atm_technician,atm_new_york):
+    atm_technician._key = 1
+    atm_technician.reload_atm(atm_new_york, 1000)
+    with pytest.raises(AttributeError):
+        atm_new_york.cash_inventory = 2000 #access denied.
+
+
+def test_block_user_from_atm(atm_new_york, nico, atm_admin):
+    atm_admin.block_user(atm_new_york, nico)
     assert nico.active is False
 
 def test_how_many_atm():
@@ -71,18 +80,15 @@ def test_how_many_atm():
 
 #  Technician Class tests
 
-def test_turn_off_and_on_atm():
+def test_turn_off_and_on_atm(atm_new_york, atm_technician):
+
+    atm_technician.turn_off_atm(atm_new_york)
+
+    assert atm_new_york.is_active == False
+
+    atm_technician.turning_on_atm(atm_new_york)
     
-    box = ATM("Colorado", 1000, _admin_key = 130)
-    george = Technician("George", _key=130)
-
-    george.turn_off_atm(box)
-
-    assert box.is_active == False
-
-    george.turning_on_atm(box)
-    
-    assert box.is_active == True
+    assert atm_new_york.is_active == True
 
 
 # Account class tests
@@ -95,19 +101,16 @@ def test_how_many_users():
     assert Account.how_many_accounts() == 1
 
 
-def test_checkfund():
+def test_checkfund(nico):
 
-    nico = Account(_pin=2026, name="Nick", _balance=10, _active=True)
+    assert nico.check_funds(500) == True
+    assert nico.check_funds(501) == False
 
-    assert nico.check_funds(10) == True
-    assert nico.check_funds(11) == False
-def test_deduct():
-
-    nico = Account(_pin=2026, name="Nick", _balance=10, _active=True)
+def test_deduct(nico):
     
     nico.deduct(10)
 
-    assert nico.balance == 0
+    assert nico.balance == 490
 
 
 
